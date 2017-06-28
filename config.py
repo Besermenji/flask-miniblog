c9edb3d28b05600067572aabe0256148fd3ba151
besermenji <besermenji.vladimir@gmail.com>
2017-06-28 22:57:41 +0200

    openId login and logout added.

--- config.py
+++ config.py
@@ -4,7 +4,6 @@ WTF_CSRF_ENABLED = True
 SECRET_KEY = os.environ["MICROBLOG_WTF_SECRET_KEY"]
 
 OPENID_PROVIDERS = [
-    {'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'},
     {'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
     {'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
     {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
