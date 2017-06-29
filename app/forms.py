d2af49107bb4748d5e5759ea4bc7cb5a29f8be4a
besermenji <besermenji.vladimir@gmail.com>
2017-06-29 16:13:01 +0200

    Edit and view profile pages added, gravatar added. about me and last seen fields added to user.

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, BooleanField
-from wtforms.validators import DataRequired
+from wtforms import StringField, BooleanField, TextAreaField
+from wtforms.validators import DataRequired, Length
 
 class LoginForm(FlaskForm):
     open_id = StringField('open_id', validators=[DataRequired()])
     remember_me = BooleanField('remember_me', default=False)
+
+class UserEditForm(FlaskForm):
+    about_me = TextAreaField('about_me', validators=[Length(min=0, max=160)])
+    nickname = StringField('nickname', validators=[DataRequired()])
