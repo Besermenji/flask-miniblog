c74d07ce55103aa12d9283406a079b0f101f4077
besermenji <besermenji.vladimir@gmail.com>
2017-07-02 11:57:54 +0200

    Duplicated user nicknames error resolved.

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, Length
+from app.models import User
 
 class LoginForm(FlaskForm):
     open_id = StringField('open_id', validators=[DataRequired()])
@@ -9,3 +10,18 @@ class LoginForm(FlaskForm):
 class UserEditForm(FlaskForm):
     about_me = TextAreaField('about_me', validators=[Length(min=0, max=160)])
     nickname = StringField('nickname', validators=[DataRequired()])
+
+    def __init__(self, original_nickname, *args, **kwargs):
+        FlaskForm.__init__(self, *args, **kwargs)
+        self.original_nickname = original_nickname
+
+    def validate(self):
+        if not FlaskForm.validate(self):
+            return False
+        if self.nickname.data == self.original_nickname:
+            return True
+        user = User.query.filter_by(nickname=self.nickname.data).first()
+        if user != None:
+            self.nickname.errors.append('This nickname is already in use. Please choose another one.')
+            return False
+        return True
--- app/views.py
+++ app/views.py
@@ -61,6 +61,7 @@ def after_login(resp):
         nickname = resp.nickname
         if nickname is None or nickname == "":
             nickname = resp.email.split('@')[0]
+        nickname = User.make_unique_nickname(nickname)
         user = User(nickname=nickname, email=resp.email)
         db.session.add(user)
         db.session.commit()
@@ -102,7 +103,7 @@ def user(nickname):
 @app.route('/user/edit', methods=['GET', 'POST'])
 @login_required
 def user_edit():
-    form = UserEditForm()
+    form = UserEditForm(g.user.nickname)
     if form.validate_on_submit():
         g.user.nickname = form.nickname.data
         g.user.about_me = form.about_me.data
