8e3c0dc05adcfab197a2336178018d86e04ee57c
besermenji <besermenji.vladimir@gmail.com>
2017-07-02 11:44:09 +0200

    404 and 500 pages added, logging errors in /tmp and sending emails if error functionality added.

--- config.py
+++ config.py
@@ -9,7 +9,17 @@ OPENID_PROVIDERS = [
     {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
     {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}]
 
+# database settings
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
+# mail server settings
+MAIL_SERVER = 'smtp.gmail.com'
+MAIL_PORT = 465
+MAIL_USERNAME = os.environ["MICROBLOG_EMAIL_USERNAME"]
+MAIL_PASSWORD = os.environ["MICROBLOG_EMAIL_PASSWORD"]
+
+# administrator list
+ADMINS = ['besermenji.vladimir@gmail.com']
